87ca8a5b6d4bf5890b27c9caca5259f81362fb37
Serge <78102903+MrXides@users.noreply.github.com>
2021-11-01 16:12:11 +0300

    T-01
    AssignExample

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int var1=4048,var2=var1/4;
+
+	System.out.println("var1= "+var1);
+	System.out.println("var2= "+var2);
+    }
+}
